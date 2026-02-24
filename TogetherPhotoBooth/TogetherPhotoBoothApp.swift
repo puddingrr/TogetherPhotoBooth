b7ef6dff448d21a4e04d4015cb58f3a42edd5d25
sugaaaaaa <ra.dalin@lomatechnology.com>
2026-02-24 10:07:00 +0700

    Initial Commit

---
+++ TogetherPhotoBooth/TogetherPhotoBoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  TogetherPhotoBoothApp.swift
+//  TogetherPhotoBooth
+//
+//  Created by Dalynn on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct TogetherPhotoBoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
